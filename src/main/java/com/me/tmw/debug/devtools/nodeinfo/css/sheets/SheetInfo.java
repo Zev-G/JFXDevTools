c1d3e004c28474f97670ea42e691c793ed963b83
TheMisterWolf <vezvez1234@gmail.com>
2021-06-06 13:30:34 -0700

    Added button to show full contents of style sheets. Fixed issue where empty rules were displayed.

--- src/main/java/com/me/tmw/debug/devtools/nodeinfo/css/sheets/SheetInfo.java
+++ src/main/java/com/me/tmw/debug/devtools/nodeinfo/css/sheets/SheetInfo.java
@@ -1,27 +1,68 @@
 package com.me.tmw.debug.devtools.nodeinfo.css.sheets;
 
 import com.me.tmw.css.Sheets;
+import javafx.css.Rule;
 import javafx.css.Stylesheet;
 import javafx.scene.Parent;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TitledPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SheetInfo extends TitledPane {
 
     private final Stylesheet stylesheet;
 
     private final VBox rules = new VBox();
+    private final BorderPane layout = new BorderPane();
+
+    private final Hyperlink viewMore = new Hyperlink("View full style sheet");
 
     public SheetInfo(Stylesheet stylesheet, Parent node) {
         this.stylesheet = stylesheet;
         String[] urlPieces = stylesheet.getUrl().split("[/\\\\]");
         setText(urlPieces[urlPieces.length - 1]);
 
-        stylesheet.getRules().stream().filter(rule -> Sheets.applies(node, rule)).forEach(rule -> {
-            rules.getChildren().add(new SheetRule(rule, node));
+        List<Rule> validRules = stylesheet.getRules().stream().filter(this::shouldAddRule).collect(Collectors.toList());
+
+        List<Rule> shownRules = new ArrayList<>();
+
+        validRules.stream().filter(rule -> Sheets.applies(node, rule)).forEach(rule -> {
+            shownRules.add(rule);
+            SheetRule sheetRule = new SheetRule(rule, node);
+            rules.getChildren().add(sheetRule);
         });
 
-        setContent(rules);
+        layout.setLeft(rules);
+
+        if (shownRules.size() < stylesheet.getRules().size()) {
+            layout.setBottom(viewMore);
+
+            viewMore.setOnAction(event -> {
+                int i = 0;
+                for (Rule rule : validRules) {
+                    if (!shownRules.contains(rule)) {
+                        SheetRule sheetRule = new SheetRule(rule, node);
+                        rules.getChildren().add(i, sheetRule);
+                        shownRules.add(rule);
+                    }
+                    i++;
+                }
+                layout.setBottom(null);
+            });
+        }
+
+        setContent(layout);
+    }
+
+    private boolean shouldAddRule(Rule rule) {
+        return rule.getDeclarations().stream()
+                .filter(declaration -> declaration.getProperty() != null)
+                .anyMatch(declaration -> declaration.getParsedValue().getConverter() != null);
     }
 
 }
